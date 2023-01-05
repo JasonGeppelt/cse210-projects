5fe0069d89e83e7c760ed986d3aed488e054f608
JasonGeppelt <jason.geppelt@gmail.com>
2023-01-05 09:30:52 -0700

    learning basic c#

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -3,7 +3,21 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Sandbox World!");
+    {   
+        // learning how to use if else
+        int x = 7;
+        int y = 8;
+        if (x > y)
+        {
+            Console.WriteLine("greater");
+        }
+        else
+        {
+            Console.WriteLine("Lesser");
+        }
+
+        // learning how to combine variables and strings
+        string school = "BYU-Idaho";
+        Console.WriteLine($"I am studying at {school}.");
     }
 }
