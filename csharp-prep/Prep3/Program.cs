3003c9c66d92f006ca5baf13921d898551f73016
JasonGeppelt <jason.geppelt@gmail.com>
2023-01-11 23:10:23 -0700

    created guess the number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // BEGINNING OF PROJECT
+
+        string keepPlaying;
+
+        do
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1,101);
+
+            int guess = -1;
+            int count = 0;
+
+            while (guess != number)
+            {
+                System.Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                count += 1;
+
+                if (guess < number)
+                {
+                    System.Console.WriteLine("Higher");
+                }
+                else if (guess > number)
+                {
+                    System.Console.WriteLine("Lower");
+                }
+                else
+                {
+                    System.Console.WriteLine("You guessed it!");
+                    System.Console.WriteLine($"It took you {count} tries.");
+                }
+            }
+
+            System.Console.Write("Keep playing? (yes/no) ");
+            keepPlaying = Console.ReadLine();
+        } while (keepPlaying == "yes");
+        // END OF PROJECT 
     }
-}
+} 
