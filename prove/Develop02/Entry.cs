3d818670f0956aeec189a274ae5cd31271a31fa7
JasonGeppelt <jason.geppelt@gmail.com>
2023-01-31 09:01:08 -0700

    created menu for journal

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,8 @@
+using System;
+
+class Entry
+{
+    public string dateAndTime;
+    public string body;
+    
+}
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        System.Console.WriteLine("Welcome to the Journal Program!");
+
+        char selection;
+        do
+        {
+            string menu = @"Please select one of the following choices:
+1. Write
+2. Display
+3. Load
+4. Save
+5. Quit";
+            System.Console.WriteLine(menu);
+            System.Console.Write("What would you like to do? ");
+            selection = char.Parse(System.Console.ReadLine());
+
+            switch(selection) 
+            {
+                case '1':
+                    // code block
+                    break;
+
+                case '2':
+                    // code block
+                    break;
+
+                case '3':
+                    System.Console.WriteLine("you chose 3.");
+                    break;
+
+                case '4':
+                    // code block
+                    break;
+
+                case '5':
+                    System.Console.WriteLine("Thank you, goodbye.");
+                    break;
+
+                default:
+                    System.Console.WriteLine("ERROR, please enter a number between 1-5.");
+                    break;
+            }
+        }
+        while (selection != '5');
     }
 }
