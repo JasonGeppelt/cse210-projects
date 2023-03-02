d97fe54f57d639bec90463ea09637fd264d000c7
JasonGeppelt <jason.geppelt@gmail.com>
2023-03-01 19:55:30 -0700

    finished prepare #5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapesList = new List<Shape>();
+
+        Square square = new Square("red", 5.0);
+        Rectangle rectangle = new Rectangle("blue", 10.0, 5.0);
+        Circle circle = new Circle("green", 7.0);
+
+        shapesList.Add(square);
+        shapesList.Add(rectangle);
+        shapesList.Add(circle);
+
+        foreach (Shape shape in shapesList)
+        {
+            System.Console.WriteLine(shape.GetColor());
+            System.Console.WriteLine(shape.GetArea());
+        }
     }
 }
